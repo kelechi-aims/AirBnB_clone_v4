43abf22c4f3f5ac0ff1ec398cec6a195237123b4
kelechi-aims <kelechi@aims.edu.gh>
2023-11-04 18:57:49 +0100

    API status

---
+++ web_dynamic/static/2-hbnb.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    var selectedAmenities = {};
+
+    $(document).on('change', "input[type='checkbox']", function() {
+        var amenityID = $(this).data('id');
+        var amenityName = $(this).data('name');
+        if ($(this).prop('checked')) {
+            selectedAmenities[amenityID] = amenityName;
+        } else {
+            delete selectedAmenities[amenityID];
+	}
+        var amenitiesList = Object.values(selectedAmenities).join(', ');
+        $('.amenities h4').text('Selected Amenities: ' + amenitiesList);
+    });
+    $.get('http://0.0.0.0:5001/api/v1/status/', function(data) {
+	if (data.status === 'OK') {
+            $('#api_status').addClass('available');
+	} else {
+            $('#api_status').removeClass('available');
+	}
+    });
+});
